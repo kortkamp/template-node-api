df204f4b70503abc3f3afa2fff5321590cdc8e51
kortkamp <marcelusmedius@gmail.com>
2022-03-16 15:53:08 -0300

    feat: add users repository

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,59 @@
+import { ICreateUserDTO } from '@modules/users/dtos/ICreateUserDTO';
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../models/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository<User>(User);
+  }
+
+  public async getTotal(): Promise<number> {
+    const result = await this.ormRepository.query(
+      'SELECT count(users.id) as total FROM users ',
+    );
+
+    return result[0].total;
+  }
+
+  public async create(data: ICreateUserDTO): Promise<User> {
+    const newUser = this.ormRepository.create(data);
+
+    await this.ormRepository.save(newUser);
+
+    return newUser;
+  }
+
+  public async getAll(): Promise<User[]> {
+    return this.ormRepository.find();
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({
+      where: { email },
+    });
+
+    return user;
+  }
+
+  public async save(data: User): Promise<void> {
+    await this.ormRepository.save(data);
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = await this.ormRepository.findOne({
+      where: { id },
+    });
+
+    return findUser;
+  }
+
+  public async delete(user: User): Promise<void> {
+    await this.ormRepository.delete(user);
+  }
+}
+
+export { UsersRepository };
---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,14 @@
+import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
+import { IUser } from '../models/IUser';
+
+interface IUsersRepository {
+  create(data: ICreateUserDTO): Promise<IUser>;
+  getAll(): Promise<IUser[]>;
+  // findById(userId: string): Promise<IUser | undefined>;
+  findByEmail(email: string): Promise<IUser | undefined>;
+  save(dataUpdate: IUser): Promise<void>;
+  delete(user: IUser): Promise<void>;
+  getTotal(): Promise<number>;
+}
+
+export { IUsersRepository };
