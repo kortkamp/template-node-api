582c62db1c84a79748a8143947145b48be7f8f01
kortkamp <marcelusmedius@gmail.com>
2022-03-16 15:57:50 -0300

    feat: add routes and error handling do app

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -7,6 +7,10 @@ import { createServer } from 'http';
 
 import databaseConnection from '@shared/infra/typeorm';
 
+import errorHandling from './middlewares/errorHandling';
+import { routes } from './routes';
+import '@shared/container';
+
 databaseConnection();
 const app = express();
 
@@ -14,6 +18,10 @@ const server = createServer(app);
 
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
+
+app.use(errorHandling);
 
 app.get('/', (req, res) => {
   return res.send('Template API Node - 2022');
