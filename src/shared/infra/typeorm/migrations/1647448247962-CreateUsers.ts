194df5398f02cdde67a23cb4fb512738a9b0d19f
kortkamp <marcelusmedius@gmail.com>
2022-03-16 15:50:12 -0300

    feat: create user migration

---
+++ src/shared/infra/typeorm/migrations/1647448247962-CreateUsers.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1647448247962 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar(255)',
+          },
+          {
+            name: 'email',
+            type: 'varchar(100)',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar(255)',
+            isNullable: false,
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+            default: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
