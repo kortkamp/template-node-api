ddbdfe37923fde82f730116eb379534c9d0dc734
kortkamp <marcelusmedius@gmail.com>
2022-03-17 16:17:01 -0300

    feat: create session route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
+import { sessionsRoutes } from '@modules/sessions/infra/http/routes/sessions.routes';
 import { usersRoutes } from '@modules/users/infra/http/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
+routes.use('/sessions', sessionsRoutes);
 
 export { routes };
