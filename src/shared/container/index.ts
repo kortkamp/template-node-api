d8895df49945c95779d9521c4fdf9117e3d7536b
kortkamp <marcelusmedius@gmail.com>
2022-03-17 16:13:55 -0300

    feat: register hashProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,12 @@ import { UsersRepository } from '@modules/users/infra/typeorm/repositories/Users
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
 import { container } from 'tsyringe';
 
+import { BCryptHashProvider } from './HashProvider/implementation/BCryptyHashProvider';
+import { IHashProvider } from './HashProvider/models/IHashProvider';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
 );
+
+container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
