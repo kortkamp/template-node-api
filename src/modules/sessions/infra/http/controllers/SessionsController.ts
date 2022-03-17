cedcb422ca59968c81782a9c5aa925ae88743dad
kortkamp <marcelusmedius@gmail.com>
2022-03-17 16:13:31 -0300

    feat: add sessions constroller

---
+++ src/modules/sessions/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,16 @@
+import { CreateSessionService } from '@modules/sessions/services/CreateSessionService';
+import { instanceToInstance } from 'class-transformer';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const createSession = container.resolve(CreateSessionService);
+
+    const session = await createSession.execute(request.body);
+
+    return response.status(201).json(instanceToInstance(session));
+  }
+}
+
+export { SessionsController };
