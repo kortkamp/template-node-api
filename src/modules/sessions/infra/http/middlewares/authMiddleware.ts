b5391f256e8e9a88e7234ad5e2627d9f07d7dd52
kortkamp <marcelusmedius@gmail.com>
2022-03-17 16:15:39 -0300

    feat: add auth middleware

---
+++ src/modules/sessions/infra/http/middlewares/authMiddleware.ts
@@ -0,0 +1,40 @@
+import { authConfig } from '@config/auth';
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import ErrorsApp from '@shared/errors/ErrorsApp';
+
+interface ITokenResponse {
+  iat: number;
+  exp: number;
+  sub: string;
+  role?: string;
+}
+
+async function authMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  const header = request.headers.authorization;
+
+  if (!header) {
+    throw new ErrorsApp('Token not provided', 401);
+  }
+  try {
+    const [, token] = header.split(' ');
+
+    const check = (await verify(
+      token,
+      authConfig.jwt.secret,
+    )) as ITokenResponse;
+
+    request.user = { id: check.sub, role: check.role || '' };
+
+    next();
+  } catch (error) {
+    throw new ErrorsApp('Invalid token', 401);
+  }
+}
+
+export { authMiddleware };
