f52bd727e3c42f109f3b910ea0c2f8c1ba9d8761
kortkamp <marcelusmedius@gmail.com>
2022-03-17 16:17:40 -0300

    feat: use authMiddleware in users routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,9 +1,12 @@
+import { authMiddleware } from '@modules/sessions/infra/http/middlewares/authMiddleware';
 import { Router } from 'express';
 
 import { UsersController } from '../controllers/UsersController';
 import { createUserValidate } from '../validations/users.validation';
 
 const usersRoutes = Router();
+
+usersRoutes.use(authMiddleware);
 
 const usersController = new UsersController();
 
