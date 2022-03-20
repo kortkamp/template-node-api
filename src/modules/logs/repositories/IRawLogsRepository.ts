834841e5b5a15bce8fc6ee58f3cd29aeff4ada59
kortkamp <marcelusmedius@gmail.com>
2022-03-20 20:22:46 -0300

    refactor: change name of arg

--- src/modules/logs/repositories/IRawLogsRepository.ts
+++ src/modules/logs/repositories/IRawLogsRepository.ts
@@ -3,5 +3,5 @@ import { IRawLog } from '../models/IRawLog';
 
 export default interface IRawLogsRepository {
   create(data: ICreateRawLogDTO): Promise<IRawLog>;
-  save(user: IRawLog): Promise<IRawLog>;
+  save(rawLog: IRawLog): Promise<IRawLog>;
 }
