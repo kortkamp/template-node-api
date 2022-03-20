21d7b73e606c8b7ecb6f80a3ab1db7b04ec83045
kortkamp <marcelusmedius@gmail.com>
2022-03-20 15:41:44 -0300

    feat: add logs respotory to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,12 @@
+import ErrorLogsRepository from '@modules/logs/infra/typeorm/repositories/ErrorLogsRepository';
+import RawLogsRepository from '@modules/logs/infra/typeorm/repositories/RawLogsRepository';
+import IErrorLogsRepository from '@modules/logs/repositories/IErrorLogsRepository';
+import IRawLogsRepository from '@modules/logs/repositories/IRawLogsRepository';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
 import { container } from 'tsyringe';
 
-import { BCryptHashProvider } from './HashProvider/implementation/BCryptyHashProvider';
+import { BCryptHashProvider } from './HashProvider/implementation/BCryptHashProvider';
 import { IHashProvider } from './HashProvider/models/IHashProvider';
 
 container.registerSingleton<IUsersRepository>(
@@ -11,3 +15,13 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
+
+container.registerSingleton<IErrorLogsRepository>(
+  'ErrorLogsRepository',
+  ErrorLogsRepository,
+);
+
+container.registerSingleton<IRawLogsRepository>(
+  'RawLogsRepository',
+  RawLogsRepository,
+);
