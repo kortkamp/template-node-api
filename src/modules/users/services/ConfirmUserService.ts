e06bd909b094c848192a3c781f95d1e5b2e9919d
kortkamp <marcelusmedius@gmail.com>
2022-03-21 21:44:12 -0300

    feat: add confirm user service

---
+++ src/modules/users/services/ConfirmUserService.ts
@@ -0,0 +1,45 @@
+import { isAfter, addHours } from 'date-fns';
+import { inject, injectable } from 'tsyringe';
+
+import ErrorsApp from '@shared/errors/ErrorsApp';
+
+import { IUsersRepository } from '../repositories/IUsersRepository';
+import { IUserTokensRepository } from '../repositories/IUserTokensRepository';
+
+@injectable()
+class ConfirmUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute(tokenId: string) {
+    const foundToken = await this.userTokensRepository.findByToken(tokenId);
+
+    if (!foundToken) {
+      throw new ErrorsApp('Token not found', 403);
+    }
+
+    const tokenExpiration = addHours(
+      foundToken.created_at,
+      Number(process.env.TOKEN_EXPIRES_IN) || 24,
+    );
+
+    if (isAfter(Date.now(), tokenExpiration)) {
+      throw new ErrorsApp('Token expired', 403);
+    }
+
+    const user = await this.usersRepository.findById(foundToken.user_id);
+
+    user.active = true;
+
+    await this.usersRepository.save(user);
+
+    await this.userTokensRepository.delete(foundToken.id);
+  }
+}
+
+export { ConfirmUserService };
