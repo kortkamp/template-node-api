69117a2e42bccd3a0bfc2d0d7833bf9d153a27d6
kortkamp <marcelusmedius@gmail.com>
2022-03-23 15:28:33 -0300

    feat: add role permission to routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,3 +1,4 @@
+import { rolePermissionsRoutes } from '@modules/roles/infra/http/routes/rolePermissions.routes';
 import { rolesRoutes } from '@modules/roles/infra/http/routes/roles.routes';
 import { sessionsRoutes } from '@modules/sessions/infra/http/routes/sessions.routes';
 import { usersRoutes } from '@modules/users/infra/http/routes/users.routes';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/roles', rolesRoutes);
+routes.use('/role-permissions', rolePermissionsRoutes);
 
 export { routes };
