341ae3a0af55de77b73778cdd398c5a72b08a810
kortkamp <marcelusmedius@gmail.com>
2022-03-23 14:11:11 -0300

    test: add create role test

---
+++ src/modules/roles/services/CreateRoleService.spec.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+
+import ErrorsApp from '@shared/errors/ErrorsApp';
+
+import { ICreateRoleDTO } from '../dtos/ICreateRoleDTO';
+import FakeRolesRepository from '../repositories/fakes/FakeRolesRepository';
+import { CreateRoleService } from './CreateRoleService';
+
+let fakeRolesRepository: FakeRolesRepository;
+
+let createRoleService: CreateRoleService;
+
+let roleData: ICreateRoleDTO;
+
+describe('CreateSessionService', () => {
+  beforeEach(() => {
+    fakeRolesRepository = new FakeRolesRepository();
+
+    createRoleService = new CreateRoleService(fakeRolesRepository);
+
+    roleData = {
+      name: 'User',
+      usersPermissions: 1,
+      rolesPermissions: 1,
+    };
+  });
+
+  it('Should be able to create a new role', async () => {
+    const role = await createRoleService.execute(roleData);
+
+    expect(role).toHaveProperty('id');
+    expect(role).toHaveProperty('name');
+
+    expect(role?.name).toBe(roleData.name);
+  });
+
+  it('Should not create 2 roles with same name ', async () => {
+    await createRoleService.execute(roleData);
+
+    await expect(createRoleService.execute(roleData)).rejects.toBeInstanceOf(
+      ErrorsApp,
+    );
+  });
+});
