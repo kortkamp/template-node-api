1f49f3b0e5a36d914b7a20b458b6e4e816b043eb
kortkamp <marcelusmedius@gmail.com>
2022-03-23 18:56:35 -0300

    refactor: change remove permission from create role

--- src/modules/roles/dtos/ICreateRoleDTO.ts
+++ src/modules/roles/dtos/ICreateRoleDTO.ts
@@ -1,11 +1,5 @@
-import { Permissions } from '../models/IRole';
-
 interface ICreateRoleDTO {
   name: string;
-
-  usersPermissions: Permissions;
-
-  rolesPermissions: Permissions;
 }
 
 export { ICreateRoleDTO };
