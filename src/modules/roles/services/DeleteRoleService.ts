c7ebaf699e36f23b87a5a9b4bbe5fb9d0396f9e5
kortkamp <marcelusmedius@gmail.com>
2022-03-23 21:41:17 -0300

    feat: add delete role service

---
+++ src/modules/roles/services/DeleteRoleService.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from 'tsyringe';
+
+import ErrorsApp from '@shared/errors/ErrorsApp';
+
+import { IRolesRepository } from '../repositories/IRolesRepository';
+
+injectable();
+class DeleteRoleService {
+  constructor(
+    @inject('RolesRepository')
+    private rolesRepository: IRolesRepository,
+  ) {}
+  public async execute(roleId: string) {
+    const role = await this.rolesRepository.findById(roleId);
+    if (!role) {
+      throw new ErrorsApp('Role does not exists', 404);
+    }
+
+    await this.rolesRepository.delete(role);
+  }
+}
+
+export { DeleteRoleService };
