27f2ec64086472ff3c2bd59b9f8b2ca0f0ba05cb
kortkamp <marcelusmedius@gmail.com>
2022-03-27 15:24:39 -0300

    feat: add delete , update and show in validations

--- src/modules/roles/infra/http/validations/roles.validation.ts
+++ src/modules/roles/infra/http/validations/roles.validation.ts
@@ -4,8 +4,32 @@ export const createRoleValidate = celebrate(
   {
     [Segments.BODY]: {
       name: Joi.string().min(3).max(100).required(),
-      usersPermissions: Joi.number().integer(),
-      rolesPermissions: Joi.number().integer(),
+    },
+  },
+  {
+    abortEarly: false,
+  },
+);
+
+export const deleteRoleValidate = celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().uuid().required(),
+  },
+});
+
+export const showRoleValidate = celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().uuid().required(),
+  },
+});
+
+export const updateRoleValidate = celebrate(
+  {
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string().min(3).max(100),
     },
   },
   {
