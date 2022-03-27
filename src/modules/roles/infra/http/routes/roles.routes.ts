200a01384daa100a7fd4fa4263a1a27e664d4410
kortkamp <marcelusmedius@gmail.com>
2022-03-27 15:24:56 -0300

    feat: add delete, update and show in routes

--- src/modules/roles/infra/http/routes/roles.routes.ts
+++ src/modules/roles/infra/http/routes/roles.routes.ts
@@ -2,7 +2,12 @@ import { authMiddleware } from '@modules/sessions/infra/http/middlewares/authMid
 import { Router } from 'express';
 
 import { RolesController } from '../controllers/RolesController';
-import { createRoleValidate } from '../validations/roles.validation';
+import {
+  createRoleValidate,
+  deleteRoleValidate,
+  showRoleValidate,
+  updateRoleValidate,
+} from '../validations/roles.validation';
 
 const rolesRoutes = Router();
 
@@ -13,5 +18,11 @@ const rolesController = new RolesController();
 rolesRoutes.post('/', createRoleValidate, rolesController.create);
 
 rolesRoutes.get('/', rolesController.index);
+
+rolesRoutes.delete('/:id', deleteRoleValidate, rolesController.delete);
+
+rolesRoutes.put('/:id', updateRoleValidate, rolesController.update);
+
+rolesRoutes.get('/:id', showRoleValidate, rolesController.show);
 
 export { rolesRoutes };
