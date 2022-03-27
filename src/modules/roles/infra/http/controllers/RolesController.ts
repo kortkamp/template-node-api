094c8fb9a9bccc39e89fd260c18d808fb1e483a2
kortkamp <marcelusmedius@gmail.com>
2022-03-27 15:24:20 -0300

    feat: add delete, update , show in controller

--- src/modules/roles/infra/http/controllers/RolesController.ts
+++ src/modules/roles/infra/http/controllers/RolesController.ts
@@ -1,5 +1,8 @@
 import { CreateRoleService } from '@modules/roles/services/CreateRoleService';
+import { DeleteRoleService } from '@modules/roles/services/DeleteRoleService';
 import { ListRolesService } from '@modules/roles/services/ListRolesService';
+import { ShowRoleService } from '@modules/roles/services/ShowRoleService';
+import { UpdateRoleService } from '@modules/roles/services/UpdateRoleService';
 import { instanceToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
@@ -19,6 +22,38 @@ class RolesController {
     const role = await createRoleService.execute(request.body);
 
     return response.status(201).json(instanceToInstance(role));
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const deleteRoleService = container.resolve(DeleteRoleService);
+
+    const roleId = request.params.id;
+
+    await deleteRoleService.execute(roleId);
+
+    return response.status(204).json({ success: true });
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateRoleService = container.resolve(UpdateRoleService);
+
+    const roleId = request.params.id;
+
+    const data = request.body;
+
+    const role = await updateRoleService.execute({ roleId, data });
+
+    return response.status(200).json({ success: true, role });
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const showRoleService = container.resolve(ShowRoleService);
+
+    const roleId = request.params.id;
+
+    const role = await showRoleService.execute(roleId);
+
+    return response.status(200).json({ success: true, role });
   }
 }
 
