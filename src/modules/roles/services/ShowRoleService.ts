7c502bcf6fe27737c951f0b9efb2a7f767fe489d
kortkamp <marcelusmedius@gmail.com>
2022-03-27 15:23:27 -0300

    feat: add show role service

---
+++ src/modules/roles/services/ShowRoleService.ts
@@ -0,0 +1,23 @@
+import { inject, injectable } from 'tsyringe';
+
+import ErrorsApp from '@shared/errors/ErrorsApp';
+
+import { IRolesRepository } from '../repositories/IRolesRepository';
+
+@injectable()
+class ShowRoleService {
+  constructor(
+    @inject('RolesRepository')
+    private rolesRepository: IRolesRepository,
+  ) {}
+  public async execute(roleId: string) {
+    const role = await this.rolesRepository.findById(roleId, ['permissions']);
+    if (!role) {
+      throw new ErrorsApp('Role does not exists', 404);
+    }
+
+    return role;
+  }
+}
+
+export { ShowRoleService };
