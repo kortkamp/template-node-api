66c399e094872899d85fec8eef78393649e1f16e
kortkamp <marcelusmedius@gmail.com>
2022-04-04 12:50:28 -0300

    feat: split routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { rolePermissionsRoutes } from '@modules/roles/infra/http/routes/rolePerm
 import { rolesRoutes } from '@modules/roles/infra/http/routes/roles.routes';
 import { sessionsRoutes } from '@modules/sessions/infra/http/routes/sessions.routes';
 import { usersRoutes } from '@modules/users/infra/http/routes/users.routes';
+import { userTokensRoutes } from '@modules/users/infra/http/routes/userTokens.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
+routes.use('/users', userTokensRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/roles', rolesRoutes);
