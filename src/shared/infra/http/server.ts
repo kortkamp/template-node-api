09ffd0d6ab8c4a4c69ec2e474e0ab7070f306338
kortkamp <marcelusmedius@gmail.com>
2022-04-08 13:42:39 -0300

    refactor: use logger instead of console.log

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,11 @@
+import { logger } from '@shared/utils/logger';
+
 import { server } from './app';
 
 const port = process.env.APP_PORT;
 
 server.on('database-ready', () => {
   server.listen(port, () => {
-    console.log(`Api started on localhost:${port}! 🚀`);
+    logger.info(`Api started on localhost:${port}! 🚀`);
   });
 });
