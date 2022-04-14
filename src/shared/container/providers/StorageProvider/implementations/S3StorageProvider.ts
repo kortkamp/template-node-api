b18d8123c98b66db809a1b8cc83b3a8f23c60525
kortkamp <marcelusmedius@gmail.com>
2022-04-14 13:16:16 -0300

    feat: add S3 Storage Provider

---
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,66 @@
+import uploadConfig from '@config/upload';
+import aws, { S3 } from 'aws-sdk';
+import fs from 'fs';
+import mime from 'mime';
+import { resolve } from 'path';
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+
+import { IDeleteFileDTO } from '../dtos/IDeleteFileDTO';
+import { ISaveFileDTO } from '../dtos/ISaveFileDTO';
+
+class S3StorageProvider implements IStorageProvider {
+  private client: S3;
+
+  constructor() {
+    this.client = new aws.S3({
+      region: process.env.AWS_REGION,
+    });
+  }
+
+  public async saveFile({
+    tmpFile,
+    fileName,
+    type,
+  }: ISaveFileDTO): Promise<string> {
+    const upConfig = uploadConfig(type);
+    const originalPath = resolve(upConfig.tmpFolder, tmpFile);
+
+    const fileContent = await fs.promises.readFile(originalPath);
+
+    const contentType = mime.getType(originalPath);
+
+    if (!contentType) {
+      throw new Error('File not found');
+    }
+
+    const { bucket } = uploadConfig(type).config.s3;
+
+    await this.client
+      .putObject({
+        Bucket: bucket,
+        Key: fileName,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType: contentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return tmpFile;
+  }
+
+  public async deleteFile({ file, type }: IDeleteFileDTO): Promise<void> {
+    const { bucket } = uploadConfig(type).config.s3;
+
+    await this.client
+      .deleteObject({
+        Bucket: bucket,
+        Key: file,
+      })
+      .promise();
+  }
+}
+
+export default S3StorageProvider;
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -4,8 +4,11 @@ import { container } from 'tsyringe';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
+import S3StorageProvider from './implementations/S3StorageProvider';
+
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
