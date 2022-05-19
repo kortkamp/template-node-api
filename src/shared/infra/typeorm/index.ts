ae2d31d54297dabe9c4c6ee98851e204afd719c8
kortkamp <marcelusmedius@gmail.com>
2022-05-19 07:20:23 -0300

    feat: use uri to database connection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,17 +7,18 @@ import 'reflect-metadata';
 const dataSourceOptions: DataSourceOptions = {
   name: process.env.POSTGRES_DB_NAME,
   type: 'postgres',
-  host:
-    process.env.NODE_ENV === 'test'
-      ? 'localhost'
-      : process.env.POSTGRES_DB_HOST,
-  port: Number(process.env.POSTGRES_DB_PORT || '5432'),
-  username: process.env.POSTGRES_DB_USERNAME,
-  password: process.env.POSTGRES_DB_PASSWORD,
-  database:
-    process.env.NODE_ENV === 'test'
-      ? 'template_test'
-      : process.env.POSTGRES_DB_DATABASE,
+  url: process.env.DATABASE_URL,
+  // host:
+  //   process.env.NODE_ENV === 'test'
+  //     ? 'localhost'
+  //     : process.env.POSTGRES_DB_HOST,
+  // port: Number(process.env.POSTGRES_DB_PORT || '5432'),
+  // username: process.env.POSTGRES_DB_USERNAME,
+  // password: process.env.POSTGRES_DB_PASSWORD,
+  // database:
+  //   process.env.NODE_ENV === 'test'
+  //     ? 'template_test'
+  //     : process.env.POSTGRES_DB_DATABASE,
   entities: [User, Role],
   migrations: [`./dist/src/shared/infra/typeorm/migrations/*.js`],
 };
