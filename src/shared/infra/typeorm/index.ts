d633c2e0afc1ff1306e40b36a56a0111dd1aa402
kortkamp <marcelusmedius@gmail.com>
2022-05-19 07:53:14 -0300

    chore: specify ssl on dbconnection

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -8,6 +8,8 @@ const dataSourceOptions: DataSourceOptions = {
   name: process.env.POSTGRES_DB_NAME,
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl:
+    process.env.ENVIRONMENT === 'prod' ? { rejectUnauthorized: false } : false,
   // host:
   //   process.env.NODE_ENV === 'test'
   //     ? 'localhost'
